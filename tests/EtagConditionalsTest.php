2ae877a26dbba375a24e4815a0056b76ea9a1e55
Christian Widlund <christian.widlund@gmail.com>
2026-03-25 23:59:30 +0100

    PHPUnit 12 Shift (#2)
    * Bump PHPUnit dependencies
    
    * Set return type of base TestCase methods
    
    From the [PHPUnit 8 release notes][1], the `TestCase` methods below now declare a `void` return type:
    
    - `setUpBeforeClass()`
    - `setUp()`
    - `assertPreConditions()`
    - `assertPostConditions()`
    - `tearDown()`
    - `tearDownAfterClass()`
    - `onNotSuccessfulTest()`
    
    [1]: https://phpunit.de/announcements/phpunit-8.html
    
    * Adopt PHP attributes in test classes
    
    * Add return types to test methods
    
    * Define test classes as `final`
    
    ---------
    
    Co-authored-by: Shift <shift@laravelshift.com>

--- tests/EtagConditionalsTest.php
+++ tests/EtagConditionalsTest.php
@@ -2,22 +2,23 @@
 
 namespace Werk365\EtagConditionals\Tests;
 
+use PHPUnit\Framework\Attributes\Test;
 use Illuminate\Http\Request;
 use Orchestra\Testbench\TestCase;
 use Symfony\Component\HttpFoundation\Response;
 use Werk365\EtagConditionals\EtagConditionals;
 
-class EtagConditionalsTest extends TestCase
+final class EtagConditionalsTest extends TestCase
 {
     private string $response = 'OK';
 
-    public function tearDown(): void
+    protected function tearDown(): void
     {
         EtagConditionals::etagGenerateUsing(null);
     }
 
-    /** @test */
-    public function get_default_etag()
+    #[Test]
+    public function get_default_etag(): void
     {
         $request = Request::create('/', 'GET');
         $response = response($this->response, 200);
@@ -25,8 +26,8 @@ class EtagConditionalsTest extends TestCase
         $this->assertEquals('"e0aa021e21dddbd6d8cecec71e9cf564"', EtagConditionals::getEtag($request, $response));
     }
 
-    /** @test */
-    public function get_etag_with_callback_md5()
+    #[Test]
+    public function get_etag_with_callback_md5(): void
     {
         $request = Request::create('/', 'GET');
         $response = response($this->response, 200);
@@ -38,8 +39,8 @@ class EtagConditionalsTest extends TestCase
         $this->assertEquals('"e0aa021e21dddbd6d8cecec71e9cf564"', EtagConditionals::getEtag($request, $response));
     }
 
-    /** @test */
-    public function get_etag_with_callback_sophisticated()
+    #[Test]
+    public function get_etag_with_callback_sophisticated(): void
     {
         $request = Request::create('/', 'GET');
         $response = response($this->response, 200);
@@ -51,8 +52,8 @@ class EtagConditionalsTest extends TestCase
         $this->assertEquals('"sophisticated"', EtagConditionals::getEtag($request, $response));
     }
 
-    /** @test */
-    public function get_etag_with_callback_with_quotes()
+    #[Test]
+    public function get_etag_with_callback_with_quotes(): void
     {
         $request = Request::create('/', 'GET');
         $response = response($this->response, 200);
